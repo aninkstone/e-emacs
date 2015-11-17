700e4d65e075f6e620710eb4feeaedd68ad68af9
Daniel C <thatways.c@gmail.com>
2015-11-17 18:21:00 +0800

    update org config

--- package.el/init-org-mode.el
+++ package.el/init-org-mode.el
@@ -102,54 +102,142 @@
 
 (setq org-publish-project-alist
       '(
-        ("org-htmls"                                                  ;Used to export .org file
-         :base-directory "~/.emacs.d/org-mode/"                  ;directory holds .org files 
-         :base-extension "org"                                        ;process .org file only    
-         :publishing-directory "~/.emacs.d/org-mode/publish-html/"            ;export destination
+        ("org-htmls"                             ;Used to export .org file
+         :base-directory "./"                    ;directory holds .org files 
+         :base-extension "org"                   ;process .org file only    
+         :publishing-directory "./publish-html/" ;export destination
          :recursive t
          :publishing-function org-html-publish-to-html
-         :headline-levels 6          ; Just the default for this project.
-         :auto-preamble nil          ; Disable auto preamble 
-         :html-preamble "<div class=\"navbar navbar-inverse navbar-fixed-top\"><div class=\"navbar-inner\"><a class=\"brand\" href=\"index.html\">Navi</a><ul class=\"nav\"><li><a href=\"index.html\">Home</a></li><li><a href=\"aboutme.html\">About</a></li></ul></div></div>"
-         :auto-sitemap t             ; Generate sitemap.org automagically...
-         :sitemap-filename "sitemap.org"    ; call it sitemap.org (it's the default)...
-         ;:sitemap-title "Sitemap"           ; with title 'Sitemap'.
-         :export-creator-info nil    ; Disable the inclusion of "Created by Org" in the postamble.
-         :export-author-info nil     ; Disable the inclusion of "Author: Your Name" in the postamble.
-         :auto-postamble nil         ; Disable auto postamble 
-         :table-of-contents nil      ; Set this to "t" if you want a table of contents, set to "nil" disables TOC.
-         :section-numbers t          ; Set this to "t" if you want headings to have numbers.
+         :html-preamble '(with-temp-buffer (insert-file-contents "./html-preamble.html") (buffer-string))
+         :table-of-contents nil
          :html-postamble ""          ; your personal postamble
-         :style-include-default nil  ; Disable the default css style
          )
-        ("org-home"                                                   ;Used to export .org file
-         :base-directory "/Volumes/E/org-base/"                         ;directory holds .org files 
-         :base-extension "org"                                        ;process .org file only    
-         :publishing-directory "publish-html/"            ;export destination
-         :recursive t
-         :publishing-function org-html-publish-to-html
-         :headline-levels 6          ; Just the default for this project.
-         :auto-preamble nil          ; Disable auto preamble 
-         :html-preamble "<div class=\"navbar navbar-inverse navbar-fixed-top\"><div class=\"navbar-inner\"><a class=\"brand\" href=\"index.html\">Navi</a><ul class=\"nav\"><li><a href=\"index.html\">Home</a></li><li><a href=\"aboutme.html\">About</a></li></ul></div></div>"
-         :auto-sitemap t                  ; Generate sitemap.org automagically...
-         :sitemap-filename "sitemap.org"    ; call it sitemap.org (it's the default)...
-         :sitemap-title "Sitemap"           ; with title 'Sitemap'.
-         :export-creator-info nil    ; Disable the inclusion of "Created by Org" in the postamble.
-         :export-author-info nil     ; Disable the inclusion of "Author: Your Name" in the postamble.
-         :auto-postamble nil         ; Disable auto postamble 
-         :table-of-contents nil      ; Set this to "t" if you want a table of contents, set to "nil" disables TOC.
-         :section-numbers t          ; Set this to "t" if you want headings to have numbers.
-         :html-postamble ""          ; your personal postamble
-         :style-include-default nil  ; Disable the default css style
-         )
-
         ("org-static"                ;Used to publish static files
-         :base-directory "~/.emacs.d/org-mode/public-html/static"
+         :base-directory "./publish-html/static"
          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-         :publishing-directory "~/.emacs.d/org-mode/publish-html/"
+         :publishing-directory "f:/Code/ogers/publish-html/"
          :recursive t
          :publishing-function org-publish-attachment
          )
 
         ("org" :components ("org-htmls" "org-static")) ;combine "org-htmls" and "org-static" into one function call
         ))
+
+(defun org-html-template (contents info)
+  "Return complete document string after HTML conversion.
+CONTENTS is the transcoded contents string.  INFO is a plist
+holding export options."
+  (concat
+   (when (and (not (org-html-html5-p info)) (org-html-xhtml-p info))
+     (let ((decl (or (and (stringp org-html-xml-declaration)
+			      org-html-xml-declaration)
+			 (cdr (assoc (plist-get info :html-extension)
+				     org-html-xml-declaration))
+			 (cdr (assoc "html" org-html-xml-declaration))
+
+			 "")))
+       (when (not (or (eq nil decl) (string= "" decl)))
+	 (format "%s\n"
+		 (format decl
+		  (or (and org-html-coding-system
+			   (fboundp 'coding-system-get)
+			   (coding-system-get org-html-coding-system 'mime-charset))
+		      "iso-8859-1"))))))
+   (org-html-doctype info)
+   "\n"
+   (concat "<html"
+	   (when (org-html-xhtml-p info)
+	     (format
+	      " xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"%s\" xml:lang=\"%s\""
+	      (plist-get info :language) (plist-get info :language)))
+	   ">\n")
+   "<head>\n"
+   (org-html--build-meta-info info)
+   (org-html--build-head info)
+   (org-html--build-mathjax-config info)
+   "</head>\n"
+   "<body>\n"
+   "
+<nav class=\"navbar navbar-default\">
+<div class=\"container-fluid\">
+<!-- Brand and toggle get grouped for better mobile display -->
+<div class=\"navbar-header\">
+    <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\" aria-expanded=\"false\">
+    <span class=\"sr-only\">Toggle navigation</span>
+    <span class=\"icon-bar\"></span>
+    <span class=\"icon-bar\"></span>
+    <span class=\"icon-bar\"></span>
+    </button>
+    <a class=\"navbar-brand\" href=\"#\">Brand</a>
+</div>
+<!-- Collect the nav links, forms, and other content for toggling -->
+<div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">
+    <ul class=\"nav navbar-nav\">
+    <li class=\"active\"><a href=\"#\">Link <span class=\"sr-only\">(current)</span></a></li>
+    <li><a href=\"#\">Link</a></li>
+    <li class=\"dropdown\">
+        <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">Dropdown <span class=\"caret\"></span></a>
+        <ul class=\"dropdown-menu\">
+        <li><a href=\"#\">Action</a></li>
+        <li><a href=\"#\">Another action</a></li>
+        <li><a href=\"#\">Something else here</a></li>
+        <li role=\"separator\" class=\"divider\"></li>
+        <li><a href=\"#\">Separated link</a></li>
+        <li role=\"separator\" class=\"divider\"></li>
+        <li><a href=\"#\">One more separated link</a></li>
+        </ul>
+    </li>
+    </ul>
+    <form class=\"navbar-form navbar-left\" role=\"search\">
+    <div class=\"form-group\">
+        <input type=\"text\" class=\"form-control\" placeholder=\"Search\">
+    </div>
+    <button type=\"submit\" class=\"btn btn-default\">Submit</button>
+    </form>
+    <ul class=\"nav navbar-nav navbar-right\">
+    <li><a href=\"#\">Link</a></li>
+    <li class=\"dropdown\">
+        <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">Dropdown <span class=\"caret\"></span></a>
+        <ul class=\"dropdown-menu\">
+        <li><a href=\"#\">Action</a></li>
+        <li><a href=\"#\">Another action</a></li>
+        <li><a href=\"#\">Something else here</a></li>
+        <li role=\"separator\" class=\"divider\"></li>
+        <li><a href=\"#\">Separated link</a></li>
+        </ul>
+    </li>
+    </ul>
+</div><!-- /.navbar-collapse -->
+</div><!-- /.container-fluid -->
+</nav>"
+
+   ;; Document container <bootstrap>
+   "<div class=\"container\">"
+   (let ((link-up (org-trim (plist-get info :html-link-up)))
+	 (link-home (org-trim (plist-get info :html-link-home))))
+     (unless (and (string= link-up "") (string= link-home ""))
+       (format org-html-home/up-format
+	       (or link-up link-home)
+	       (or link-home link-up))))
+   ;; Preamble.
+   (org-html--build-pre/postamble 'preamble info)
+   ;; Big head
+   "<div class=\"jumbotron\">"
+   "<h1>Hello, world!</h1>"
+   "<p>...</p>"
+   "<p><a class=\"btn btn-primary btn-lg\" href=\"#\" role=\"button\">Learn more</a></p>"
+   "</div>"
+   ;; Document contents.
+   (format "<%s id=\"%s\">\n"
+	   (nth 1 (assq 'content org-html-divs))
+	   (nth 2 (assq 'content org-html-divs)))
+   ;; Document title.
+   (let ((title (plist-get info :title)))
+     (format "<h1 class=\"title\">%s</h1>\n" (org-export-data (or title "") info)))
+   contents
+   (format "</%s>\n"
+	   (nth 1 (assq 'content org-html-divs)))
+   ;; Postamble.
+   (org-html--build-pre/postamble 'postamble info)
+   ;; Closing document.
+   "</div></body>\n</html>"))
