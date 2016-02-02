a84f6181beaf97ad7883c7381565fc5963b6492f
Daniel C <thatways.c@gmail.com>
2016-02-02 16:46:00 +0800

    disable semantic idle sync

--- package.el/init-ede-mode.el
+++ package.el/init-ede-mode.el
@@ -1,3 +1,11 @@
 (require 'cedet)
-(semantic-mode)
-(global-semantic-idle-summary-mode)
+(require 'semantic/ia)
+(require 'semantic/bovine/gcc)
+
+(global-ede-mode t)
+(semantic-mode t)
+;;(global-semantic-idle-summary-mode)
+
+(setq qt5-base-dir "/usr/include/qt5")
+(semantic-add-system-include qt5-base-dir 'c++-mode)
+(add-to-list 'auto-mode-alist (cons qt5-base-dir 'c++-mode))
